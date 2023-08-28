c7b1870bac13f287d02fd543d67fb85f0134ae10
imxaander <xander22.ison@gmail.com>
2023-08-28 14:59:00 +0800

    medj done

--- docs/images-to-pdf/scripts/script.js
+++ docs/images-to-pdf/scripts/script.js
@@ -1,9 +1,14 @@
+
+
 const fileInput = document.getElementById("files-input");
 
-function downloadPDF(){
+function PDF(action){
+    console.log("PDF is Called.");
     let orientation = document.getElementById("files-o").value
     let unit = document.getElementById("files-u").value
     let format = document.getElementById("files-f").value
+    var filename = document.getElementById("file-name").value
+    var margin = parseFloat(document.getElementById("files-margin").value)
     var pdfConfig = {
         orientation: orientation,
         unit: unit,
@@ -16,19 +21,20 @@ function downloadPDF(){
     //loop logic
     var files = fileInput.files
     var img = new Image()
-    if (files.length != 0) {
-        
+    if (files.length != 0){
         for (let index = 0; index < files.length; index++) {
             console.log(files[index]);
             doc.addPage()
             img.src = URL.createObjectURL(files[index])
-            console.log(img.src);
-            doc.addImage(img, 'png',0, 0, 10, 10)
-    
-        }
-        doc.save("wow")
-    }else{
+            let docwidth = doc.internal.pageSize.getWidth()
+            let docheight = doc.internal.pageSize.getHeight()
 
+            doc.addImage(img, 'png', margin, margin, docwidth-(2*margin), docheight-(2*margin))
+        }
+        if (action == "download") {
+            //doc.save(filename+".pdf")
+        }
+        document.getElementById('main-iframe').setAttribute('src', doc.output('bloburl'));
     }
 }
 

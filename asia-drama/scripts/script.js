fd40772336abb337fd270e0ab062b99e66cc73ce
imxaander <xander22.ison@gmail.com>
2023-12-17 20:05:16 +0800

    iu

--- asia-drama/scripts/script.js
+++ asia-drama/scripts/script.js
@@ -32,21 +32,48 @@ function search(){
 }
 
 var movieModal = new bootstrap.Modal(document.getElementById('item-modal'));
-
+var movieHeader =document.getElementsByClassName("modal-trailer")[0];
 function detail(id, name){
+    movieHeader.innerHTML = "";
     document.getElementById("item-title").innerHTML = name;
     var wrapper = document.getElementById('item-episodes');
+    
     wrapper.innerHTML = '<i class="fas fa-spinner spinner"></i>';
+    
     fetch('https://dramacool.com.pa/drama-detail/'+id).then(response => response.text()).then(html =>{
         var page = document.createElement('html');
         page.innerHTML = html;
 
         var tab = document.createElement("div");
         tab.innerHTML = page.getElementsByClassName("tab-content left-tab-1")[0].outerHTML;
+
+        let trailer;
+        let result = true;
+        try{
+            page.querySelector(".trailer").querySelector("iframe").src;
+        }catch(err){
+            result = false;
+        }
+        if(result){
+            trailer = page.querySelector(".trailer").querySelector("iframe").src;
+            movieHeader.innerHTML += `      
+            <iframe 
+            class="bg-video"
+            src="${trailer}?rel=0?version=3&autoplay=1&controls=0&&showinfo=0&loop=1"
+            frameBorder="0"
+            allowFullScreen
+            allow="autoplay"
+            />`;
+        }else{
+            movieHeader.innerHTML += `<p> Trailer not available </p`;
+
+        }
+
         
         var ul = tab.querySelector("ul");
-
+        
         wrapper.innerHTML = "";
+        
         for(let child of Array.from(ul.children)){
 
             let item = document.createElement("div");
@@ -57,7 +84,7 @@ function detail(id, name){
             item.setAttribute("class", `12`);
             item.innerHTML = `Episode ${number}:  ${date}`;
             
-            console.log(child)
+            console.log(trailer)
             wrapper.appendChild(item);
         }
     });
@@ -81,6 +108,8 @@ function watch(id){
         wrapper.innerHTML = `<iframe id="video-player-iframe" sandbox = "allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation" width="100%" height="100%" allowfullscreen src="${url}" target="_blank"></iframe>`;
     });
 }
+
+
 /*
 
 

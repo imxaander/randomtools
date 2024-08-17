3bb5717ba9950ef0337702cfee2b602cca1f7ea1
Xander Ison <73865197+imxaander@users.noreply.github.com>
2024-08-17 13:41:26 +0800

    Update gfsday.js

--- asia-drama/gfsday.js
+++ asia-drama/gfsday.js
@@ -3,7 +3,6 @@ var gfModal = new bootstrap.Modal(document.getElementById('gfsday'), {})
 
 if(localStorage.getItem("saw-gf") == null){
   if(prompt("Guess the pin :D. (in numbers)") == '021419'){
-    gfModal.show()
     localStorage.setItem("saw-gf", true);
   }
 }
